7e65605d7799899d1827f2de2705f571eb4f8b23
GUchechi <godswilluchendu7@gmail.com>
2024-08-18 14:54:58 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 let navbar = document.querySelector(".header .navbar");
 let menuBtn = document.querySelector("#menu-btn");
+let navLinks = document.querySelectorAll(".header .navbar a");
 
 menuBtn.onclick = () => {
   menuBtn.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
 
-window.scroll = () => {
-  menuBtn.classList.remove("fa-times");
-  menuBtn.classList.remove("active");
-};
+// Close navbar when a link is clicked
+navLinks.forEach((link) => {
+  link.onclick = () => {
+    navbar.classList.remove("active");
+    menuBtn.classList.remove("fa-times");
+  };
+});
 
+// Close navbar when clicking outside on smaller screens
+window.onclick = (event) => {
+  if (
+    !event.target.matches(".fa-bars") &&
+    navbar.classList.contains("active")
+  ) {
+    navbar.classList.remove("active");
+    menuBtn.classList.remove("fa-times");
+  }
+};
